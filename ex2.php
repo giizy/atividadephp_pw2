bdfa7ac5d64b83806b8955182553760868a1023b
giizy <giovanna.pm2020@gmail.com>
2025-03-31 20:22:38 -0300

    atv continuação

--- ex2.php
+++ ex2.php
@@ -1,5 +1,5 @@
 <?php 
-$n = 10;
+$n = 36;
 
 	if ($n%2==0 && $n%5==0 && $n%10==0) {
 		echo "Número divisível por 2, 5 e 10";
@@ -7,7 +7,9 @@ $n = 10;
 	elseif ($n%5==0 && $n%10==0) {
 		echo "Número divisível por 5 e 10";}
 	elseif ($n%10==0) {
-		echo "Número divisível por 5 e 10";}
+		echo "Número divisível por 10";}
+	elseif ($n%2==0) {
+		echo "Número divisível por 2";}
 	else{
 		echo "Não é divisível por nenhuma das opções.";
 	}
---
+++ ex5.php
@@ -0,0 +1,12 @@
+<?php 
+	
+	$idade; $fulano;
+	for ($cont=0; $cont<=50; $cont++) {
+		echo "</br>altura ".$cont;}
+		for ($cont=0; $cont<=50; $cont++) {
+		echo "</br>idade ".$cont;}
+			for ($cont=0; $cont<=50; $cont++) {
+			echo "</br>sexo ".$cont;}
+	
+
+ ?>
